caf92642656e8170a0dfda4608b5f45fe4cd1de6
Lutando Ngqakaza <lutando@ngqakaza.com>
2016-08-21 11:22:36 +0200

    project scaffolding pt2 done

---
+++ src/TFN.Api/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+
+namespace TFN.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseUrls("http://localhost:5000")
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
