626b3743988dd01d7c74b6540087fb4e253d6e41
Lutando Ngqakaza <lutando@ngqakaza.com>
2016-08-21 11:28:17 +0200

    added login ui elements

---
+++ src/TFN.Api/UI/Login/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Host.UI.Login
+{
+    public class LoginViewModel : LoginInputModel
+    {
+        public LoginViewModel()
+        {
+        }
+
+        public LoginViewModel(LoginInputModel other)
+        {
+            Username = other.Username;
+            Password = other.Password;
+            RememberLogin = other.RememberLogin;
+            ReturnUrl = other.ReturnUrl;
+        }
+
+        public string ErrorMessage { get; set; }
+    }
+}
