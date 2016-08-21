c95e09ccb4f06a59cf626d0d32325b0b576a06a9
Lutando Ngqakaza <lutando@ngqakaza.com>
2016-08-21 13:20:16 +0200

    Added Domain Driven Design Helper Library Project. Setup Resolver Class

---
+++ src/TFN.Domain.Resolution/Resolver.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TFN.Domain.Resolution
+{
+    public class Resolver
+    {
+        public void RegisterTypes(IServiceCollection services)
+        {
+        }
+
+        public void RegisterAuthorizationPolicies(IServiceCollection services)
+        {
+        }
+    }
+}
