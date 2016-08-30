fd91b8cc68e0dea1904fa8c4f38ce3632197247c
Lutando Ngqakaza <lutz@whereismytransport.com>
2016-08-30 10:01:46 +0200

    - added some domain props to entities

--- src/TFN.Domain.Models/Entities/Post.cs
+++ src/TFN.Domain.Models/Entities/Post.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NodaTime;
 using TFN.DomainDrivenArchitecture.Domain.Models;
 
 namespace TFN.Domain.Models.Entities
 {
     public class Post : DomainEntity<Guid>
     {
+        public string TrackUrl { get; private set; }
+        public string Text { get; private set; }
+        public Instant Created { get; private set; }
+        public Instant Modified { get; private set; }
+        public int Likes { get; private set; }
+        public IReadOnlyCollection<string> Tags { get; private set; }
+        public Post(Guid id)
+            : base(id)
+        {
+            
+        }
     }
 }
---
+++ src/TFN.Domain.Models/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TFN.DomainDrivenArchitecture.Domain.Models;
+
+namespace TFN.Domain.Models.Entities
+{
+    public class User : DomainEntity<Guid>
+    {
+        public Guid Id { get; private set; }
+        public string Username { get; set; }
+        public IReadOnlyCollection<Claim> Claims { get; set; }
+        public User(Guid id)
+            : base(id)
+        {
+            
+        }
+    }
+}
