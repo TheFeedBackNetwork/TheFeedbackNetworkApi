093763c1a1d6d33d3438c00e9b026c463620439a
Lutando Ngqakaza <lutz@whereismytransport.com>
2016-08-30 09:18:20 +0200

    - Fixed the build

--- src/TFN.Domain.Models/Entities/Comment.cs
+++ src/TFN.Domain.Models/Entities/Comment.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TFN.DomainDrivenArchitecture.Domain.Models;
 
 namespace TFN.Domain.Models.Entities
 {
-    public class Comment
+    public class Comment : DomainEntity<Guid>
     {
+        public Comment(Guid id)
+            : base(id)
+        {
+            
+        }
+
     }
 }
