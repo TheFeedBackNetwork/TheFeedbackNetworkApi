b4e0a04f5a7e0dc7e63325c0c6f91eaa00faa8a8
Lutando Ngqakaza <lutz@whereismytransport.com>
2016-08-30 08:56:39 +0200

    -- added proto domain entitites

---
+++ src/TFN.Domain.Models/Entities/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TFN.DomainDrivenArchitecture.Domain.Models;
+
+namespace TFN.Domain.Models.Entities
+{
+    public class Post : DomainEntity<Guid>
+    {
+    }
+}
