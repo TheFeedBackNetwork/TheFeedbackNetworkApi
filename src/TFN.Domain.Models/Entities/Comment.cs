35642bce6e5afa97726c344a549b813efcdd56dc
Lutando Ngqakaza <lutz@whereismytransport.com>
2016-09-01 15:03:40 +0200

    - refactored some of the classes

--- src/TFN.Domain.Models/Entities/Comment.cs
+++ src/TFN.Domain.Models/Entities/Comment.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using NodaTime;
-using TFN.DomainDrivenArchitecture.Domain.Models;
 
 namespace TFN.Domain.Models.Entities
 {
--- src/TFN.Domain.Models/Entities/User.cs
+++ src/TFN.Domain.Models/Entities/User.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
 using NodaTime;
 using TFN.Domain.Models.ValueObjects;
 using TFN.DomainDrivenArchitecture.Domain.Models;
@@ -12,36 +8,36 @@ namespace TFN.Domain.Models.Entities
     public class User : DomainEntity<Guid>
     {
         public string Username { get; private set; }
-        public string ProfilePicture { get; private set; } 
+        public string ProfilePictureUrl { get; private set; } 
         public string Email { get; private set; }
         public string GivenName { get; private set; }
         public string FamilyName { get; private set; }
         public Biography Biography { get; private set; }
         public Instant Created { get; private set; }
 
-        public User(string username,string profilePicture, string email, string givenName, string familyName, Biography biography, Instant created)
-            : this(Guid.NewGuid(), username,profilePicture, email, givenName, familyName, biography,created)
+        public User(string username,string profilePictureUrl, string email, string givenName, string familyName, Biography biography, Instant created)
+            : this(Guid.NewGuid(), username,profilePictureUrl, email, givenName, familyName, biography,created)
         {
             
         }
-        private User(Guid id, string username, string profilePicture, string email, string givenName, string familyName, Biography biography, Instant created)
+        private User(Guid id, string username, string profilePictureUrl, string email, string givenName, string familyName, Biography biography, Instant created)
             : base(id)
         {
             //TODO Domain Validation
 
             Username = username;
-            ProfilePicture = profilePicture;
             Email = email;
+            ProfilePictureUrl = profilePictureUrl;
             GivenName = givenName;
             FamilyName = familyName;
             Biography = biography;
             Created = created;
         }
 
-        public static User Hydrate(Guid id, string username, string profilePicture, string email, string givenName, string familyName, Biography biography,
+        public static User Hydrate(Guid id, string username, string profilePictureUrl, string email, string givenName, string familyName, Biography biography,
             Instant created)
         {
-            return new User(id, username,profilePicture, email, givenName, familyName, biography, created);
+            return new User(id, username,profilePictureUrl, email, givenName, familyName, biography, created);
         }
     }
 }
