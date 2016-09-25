bcbcdffc80028e17b236e14a1562e49b6a80ca6d
Lutando Ngqakaza <lutando@ngqakaza.com>
2016-09-25 16:40:10 +0200

    IDS4 Configuration
    - Added clients
    - Added scopes

--- src/TFN.Infrastructure/Repositories/ClientAggregate/InMemory/InMemoryClients.cs
+++ src/TFN.Infrastructure/Repositories/ClientAggregate/InMemory/InMemoryClients.cs
@@ -2,10 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4.Models;
 
 namespace TFN.Infrastructure.Repositories.ClientAggregate.InMemory
 {
     public class InMemoryClients
     {
+        public static IEnumerable<Client> Clients = new List<Client>
+        {
+            new Client
+            {
+                ClientId = "tfn_postman",
+                ClientName = "TFN Postman Client",
+                IncludeJwtId = true,
+                PrefixClientClaims = true,
+                AccessTokenType = AccessTokenType.Jwt,
+                AlwaysSendClientClaims = true,
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                AllowedScopes = new List<string>
+                {
+                    "openid",
+                    "profile",
+                    "biography",
+                    "profile_picture_url",
+                    "posts.write",
+                    "posts.read",
+                    "posts.edit",
+                    "posts.delete"
+                }
+            },
+            new Client
+            {
+                ClientId = "tfn_frontend",
+                ClientName = "TFN Frontent Client",
+                IncludeJwtId = true,
+                PrefixClientClaims = true,
+                AccessTokenType = AccessTokenType.Jwt,
+                AllowAccessTokensViaBrowser = true,
+                AlwaysSendClientClaims = true,
+                AllowedGrantTypes = GrantTypes.Implicit,
+                AllowedScopes = new List<string>
+                {
+                    "openid",
+                    "profile",
+                    "biography",
+                    "profile_picture_url",
+                    "posts.write",
+                    "posts.read",
+                    "posts.edit",
+                    "posts.delete"
+                }
+            },
+        };
     }
 }
--- src/TFN.Infrastructure/Repositories/ScopeAggregate/InMemory/InMemoryScopes.cs
+++ src/TFN.Infrastructure/Repositories/ScopeAggregate/InMemory/InMemoryScopes.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using IdentityModel;
 using IdentityServer4.Models;
 
 namespace TFN.Infrastructure.Repositories.ScopeAggregate.InMemory
@@ -13,23 +11,86 @@ namespace TFN.Infrastructure.Repositories.ScopeAggregate.InMemory
             StandardScopes.OpenId,
             StandardScopes.ProfileAlwaysInclude,
             StandardScopes.RolesAlwaysInclude,
-
             new Scope
+            {
+                Name = "biography",
+                Description = "Non standard identity scope for holding biography information",
+                Type = ScopeType.Identity
+            },
+            new Scope
+            {
+                Name = "profile_picture_url",
+                Description = "Non standard identity scope for holding profile picture url",
+                Type = ScopeType.Identity
+            },
+            new Scope
+            {
+                Name = "posts.read",
+                DisplayName = "Posts Read",
+                Description = "Scope for retreiving the posts resource",
+                Type = ScopeType.Resource,
+                Claims = new List<ScopeClaim>
                 {
-                    Name = "api1",
-                    DisplayName = "API 1",
-                    Description = "API 1 features and data",
-                    Type = ScopeType.Resource,
-
-                    ScopeSecrets = new List<Secret>
-                    {
-                        new Secret("secret".Sha256())
-                    },
-                    Claims = new List<ScopeClaim>
-                    {
-                        new ScopeClaim("role")
-                    }
-                },
+                    new ScopeClaim(JwtClaimTypes.Email, true),
+                    new ScopeClaim(JwtClaimTypes.PreferredUserName, true),
+                    new ScopeClaim(JwtClaimTypes.Picture, true),
+                    new ScopeClaim(JwtClaimTypes.GivenName, true),
+                    new ScopeClaim(JwtClaimTypes.FamilyName, true),
+                    new ScopeClaim("biography", true),
+                    new ScopeClaim("profile_picture_url", true),
+                }
+            },
+            new Scope
+            {
+                Name = "posts.write",
+                DisplayName = "Posts Write",
+                Description = "Scope for writing to the posts resource",
+                Type = ScopeType.Resource,
+                Claims = new List<ScopeClaim>
+                {
+                    new ScopeClaim(JwtClaimTypes.Email, true),
+                    new ScopeClaim(JwtClaimTypes.PreferredUserName, true),
+                    new ScopeClaim(JwtClaimTypes.Picture, true),
+                    new ScopeClaim(JwtClaimTypes.GivenName, true),
+                    new ScopeClaim(JwtClaimTypes.FamilyName, true),
+                    new ScopeClaim("biography", true),
+                    new ScopeClaim("profile_picture_url", true),
+                }
+            },
+            new Scope
+            {
+                Name = "posts.edit",
+                DisplayName = "Posts Edit",
+                Description = "Scope for editing the posts resource",
+                Type = ScopeType.Resource,
+                Claims = new List<ScopeClaim>
+                {
+                    new ScopeClaim(JwtClaimTypes.Email, true),
+                    new ScopeClaim(JwtClaimTypes.PreferredUserName, true),
+                    new ScopeClaim(JwtClaimTypes.Picture, true),
+                    new ScopeClaim(JwtClaimTypes.GivenName, true),
+                    new ScopeClaim(JwtClaimTypes.FamilyName, true),
+                    new ScopeClaim("biography", true),
+                    new ScopeClaim("profile_picture_url", true),
+                }
+            },
+            new Scope
+            {
+                Name = "posts.delete",
+                DisplayName = "Posts Edit",
+                Description = "Scope for deleting the posts resource",
+                Type = ScopeType.Resource,
+                Claims = new List<ScopeClaim>
+                {
+                    new ScopeClaim(JwtClaimTypes.Email, true),
+                    new ScopeClaim(JwtClaimTypes.PreferredUserName, true),
+                    new ScopeClaim(JwtClaimTypes.Picture, true),
+                    new ScopeClaim(JwtClaimTypes.GivenName, true),
+                    new ScopeClaim(JwtClaimTypes.FamilyName, true),
+                    new ScopeClaim("biography", true),
+                    new ScopeClaim("profile_picture_url", true),
+                }
+            }
         };
     }
 }
