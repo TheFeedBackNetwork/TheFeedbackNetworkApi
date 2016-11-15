e082f1b07565bb7369013e4b5030b5d5c9e10b0e
Lutando Ngqakaza <lutando@ngqakaza.com>
2016-11-15 06:22:46 +0200

    Tried

---
+++ src/TFN.Infrastructure/Modules/LoggerFactoryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using TFN.Infrastructure.Modules.Logging.AppendBlobLogger;
+
+namespace TFN.Infrastructure.Modules
+{
+    public static class LoggerFactoryExtensions
+    {
+        public static ILoggerFactory AddAppendBlob(
+            this ILoggerFactory factory,
+            string connectionString,
+            LogLevel minimumLevel)
+        {
+            factory.AddProvider(new AppendBlobLoggerProvider(connectionString, minimumLevel));
+            return factory;
+        }
+    }
+}
---
+++ src/TFN.Infrastructure/Modules/Logging/AppendBlobLogger/AppendBlobLoggerProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace TFN.Infrastructure.Modules.Logging.AppendBlobLogger
+{
+    internal class AppendBlobLoggerProvider : ILoggerProvider
+    {
+        public LogLevel MinimumLevel { get; }
+
+        public string StorageAccountConnectionString { get; }
+
+        public AppendBlobLoggerProvider(string storageAccountConnectionString, LogLevel minimumLevel)
+        {
+            StorageAccountConnectionString = storageAccountConnectionString;
+            MinimumLevel = minimumLevel;
+        }
+
+        public ILogger CreateLogger(string name)
+        {
+            return new AppendBlobLogger(StorageAccountConnectionString, MinimumLevel, name);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
