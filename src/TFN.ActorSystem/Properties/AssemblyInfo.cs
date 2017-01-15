f45d2e984b087c486b3026a64bb08c2f90d8ae24
Lutando Ngqakaza <lutando@ngqakaza.com>
2017-01-15 11:31:12 +0200

    Actor System init
    Added dependencies and references to Akka

---
+++ src/TFN.ActorSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TFN.ActorSystem")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b655e5e9-9a04-45fe-b2b4-1fe675621d63")]
---
+++ src/TFN.ActorSystem/SystemActors.cs
@@ -0,0 +1,10 @@
+﻿using Akka.Actor;
+
+namespace TFN.ActorSystem
+{
+    public static class SystemActors
+    {
+        public static IActorRef UserSystemActor;
+        public static IActorRef PostsSystemActor;
+    }
+}
