aaa6153eb8a27f26f35a09506b2f5190380dcc6b
Lutando Ngqakaza <lutando@ngqakaza.com>
2017-02-15 05:58:02 +0200

    some refactor

--- src/TFN.Api/UI/VerifyAccount/VerifyInputModel.cs
+++ src/TFN.Api/UI/VerifyAccount/VerifyInputModel.cs
@@ -9,6 +9,6 @@ namespace TFN.Api.UI.VerifyAccount
         [StringLength(200, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
         [PasswordValid(MinLetters = 1, MinDigits = 1, ErrorMessage = "Password must contain at least one number and one letter.")]
         public string VerifyPassword { get; set; }
-        public string VerificationKey { get; set; }
+        public string EmailVerificationKey { get; set; }
     }
 }
