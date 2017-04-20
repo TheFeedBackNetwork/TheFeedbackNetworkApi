a979687654730dd5083d159d344b85b491b351a6
Lutando Ngqakaza <lutando@ngqakaza.com>
2017-04-20 08:04:46 +0200

    tc pp

---
+++ src/TFN.Api/UI/PrivacyPolicy/PrivacyPolicyController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TFN.Api.UI.PrivacyPolicy
+{
+    public class PrivacyPolicyController : Controller
+    {
+        [HttpGet]
+        [Route("privacy-policy", Name = "PrivacyPolicy")]
+        public IActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/TFN.Api/UI/TermsConditions/TermsConditionsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TFN.Api.UI.TermsConditions
+{
+    public class TermsConditionsController : Controller
+    {
+        [HttpGet]
+        [Route("terms-conditions", Name = "TermsConditions")]
+        public IActionResult TermsConditions()
+        {
+            return View();
+        }
+    }
+}
